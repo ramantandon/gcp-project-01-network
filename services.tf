dbc48757948021716b445385d9fd5c6c0c89eee2
Raman Tandon <raman@ramantandon.com>
2026-02-06 12:08:30 -0500

    Update services

--- services.tf
+++ services.tf
@@ -4,46 +4,46 @@ resource "google_project_service" "compute" {
 }
 
 # resource "google_project_service" "billing" {
-#   project = var.project
+#   project = var.project_id
 #   service = "cloudbilling.googleapis.com"
 # }
 
 # resource "google_project_service" "iam" {
-#   project = var.project
+#   project = var.project_id
 #   service = "iam.googleapis.com"
 # }
 
 # resource "google_project_service" "storage" {
-#   project = var.project
+#   project = var.project_id
 #   service = "storage-component.googleapis.com"
 # }
 
 # resource "google_project_service" "storage-json" {
-#   project = var.project
+#   project = var.project_id
 #   service = "storage-api.googleapis.com"
 # }
 
 # resource "google_project_service" "cloudresourcemanager" {
-#   project = var.project
+#   project = var.project_id
 #   service = "cloudresourcemanager.googleapis.com"
 # }
 
 # resource "google_project_service" "serviceusage" {
-#   project = var.project
+#   project = var.project_id
 #   service = "serviceusage.googleapis.com"
 # }
 
 # resource "google_project_service" "cloudkms" {
-#   project = var.project
+#   project = var.project_id
 #   service = "cloudkms.googleapis.com"
 # }
 
 # resource "google_project_service" "containerregistry" {
-#   project = var.project
+#   project = var.project_id
 #   service = "containerregistry.googleapis.com"
 # }
 
 # resource "google_project_service" "artifactregistry" {
-#   project = var.project
+#   project = var.project_id
 #   service = "artifactregistry.googleapis.com"
 # }
